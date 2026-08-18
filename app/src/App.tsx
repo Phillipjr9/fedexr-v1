544e9c41e304090d54110cc4618d2c84b73111a8
Phillipjr9 <phillipjr258@gmail.com>
2026-08-18 18:00:04 -0400

    Add /admin/holds route

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -22,6 +22,7 @@ import AdminUsers from '@/pages/admin/AdminUsers';
 import AdminActivity from '@/pages/admin/AdminActivity';
 import AdminLocations from '@/pages/admin/AdminLocations';
 import AdminReports from '@/pages/admin/AdminReports';
+import AdminHolds from '@/pages/admin/AdminHolds';
 import CreateShipmentPage from '@/pages/CreateShipmentPage';
 import UserDashboard from '@/components/dashboard/UserDashboard';
 import SuppliesStore from '@/components/store/SuppliesStore';
@@ -89,6 +90,7 @@ function AppRoutes() {
         <Route path="/admin" element={<AdminLayout />}>
           <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="shipments" element={<AdminShipments />} />
+          <Route path="holds" element={<AdminHolds />} />
           <Route path="users" element={<AdminUsers />} />
           <Route path="locations" element={<AdminLocations />} />
           <Route path="banner" element={<AdminBanner />} />
